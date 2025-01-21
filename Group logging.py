b5c05a6e077b44734ec68ca0e085bdfb3dd223f9
rvmeer <rvmeer>
2025-01-21 09:42:26 +0000

    Added some additional features

--- Group logging.py
+++ Group logging.py
@@ -1,11 +1,11 @@
 # Databricks notebook source
 # MAGIC %sql
-# MAGIC SELECT * FROM `experiments3`.`logging`.`cidt_lite_logging_last_24_hours_2025_01_20`;
+# MAGIC SELECT * FROM `experiments3`.`logging`.`combined`;
 
 # COMMAND ----------
 
 # Display the min and max date from the _time column
-max_dates = spark.sql("SELECT MIN(_time) as min_time, MAX(_time) as max_time FROM _sqldf")
+max_dates = spark.sql("SELECT MIN(iso_timestamp) as min_time, MAX(iso_timestamp) as max_time FROM _sqldf")
 
 display(max_dates)
 
@@ -19,7 +19,7 @@ df_filtered = spark.sql("SELECT * FROM _sqldf WHERE cpd_name IS NOT NULL")
 # From the filtered df, show the unique cidt_version, user and cpd_name with the count of events
 df_grouped = df_filtered.groupBy("cidt_version", "user", "cpd_name").count()
 
-df_grouped.show()
+df_grouped.show(1000,False)
 
 
 # COMMAND ----------
@@ -31,7 +31,12 @@ df_grouped.write.format("delta").mode("overwrite").saveAsTable("experiments3.log
 
 # COMMAND ----------
 
-# Now read the grouped table back in
-df_grouped_delta = spark.read.format("delta").load("/tmp/delta/grouped")
+# From the filtered df, where feature is not the string 'NULL', group by user, cpd_name and feature and count the events
+df_grouped_with_feature = df_filtered.where("feature <> 'NULL'").groupBy("user", "cpd_name", "feature").count()
 
-df_grouped_delta.show()
+df_grouped_with_feature.show(1000,False)
+
+# COMMAND ----------
+
+df_grouped_with_feature.write.format("delta").mode("overwrite").saveAsTable("experiments3.logging.grouped_with_feature")
+
